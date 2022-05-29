50e49f47bbf191b99689a4a6e616d5eb47888d53
Nahuel Genchi <nagen01@gmail.com>
2022-05-29 15:54:07 -0300

    Showing random paragraph as a typing text

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-const typingText = document.querySelector(".typing-text");
+const typingText = document.querySelector(".typing-text p"),
+inpField = document.querySelector(".wrapper .input-field");
+
+let charIndex = 0;
+
+function randomParagraph() {
+    // getting random number and it'll always less than the paragraphs length
+    let randIndex = Math.floor(Math.random() * paragraphs.length);
+    /* getting random item from the paragraphs array, splitting all characters
+    of it, adding each character inside span and then adding this span inside p tag */
+    paragraphs[randIndex].split("").forEach(span => {
+        let spanTag = `<span>${span}</span>`;
+        typingText.innerHTML += spanTag;
+    });
+    // focusing input-field on keydown or click event
+    document.addEventListener("keydown", () => inpField.focus());
+    typingText.addEventListener("click", () => inpField.focus());
+};
+
+function initTyping() {
+    const characters = typingText.querySelectorAll("span");
+    let typedChar = inpField.value.split("")[charIndex];
+    console.log(typedChar);
+};
+
+randomParagraph();
+inpField.addEventListener("input", initTyping);
